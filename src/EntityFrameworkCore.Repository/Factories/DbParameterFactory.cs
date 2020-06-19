25280fe0bb30159ab2ce66c04c76d92e22fc6f11
ffernandolima <fernando.lima@warrenbrasil.com.br>
2020-06-19 14:15:50 -0300

    Created DbParameterFactory.

---
+++ src/EntityFrameworkCore.Repository/Factories/DbParameterFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+
+namespace EntityFrameworkCore.Repository.Factories
+{
+    public static class DbParameterFactory
+    {
+        public static DbParameter CreateDbParameter<TParameter>(string parameterName, object parameterValue, ParameterDirection direction = ParameterDirection.Input) where TParameter : DbParameter
+        {
+            if (string.IsNullOrWhiteSpace(parameterName))
+            {
+                throw new ArgumentException($"{nameof(parameterName)} cannot be null or white-space.", nameof(parameterName));
+            }
+
+            var parameter = Activator.CreateInstance<TParameter>();
+            {
+                parameter.ParameterName = parameterName;
+                parameter.Value = parameterValue ?? DBNull.Value;
+                parameter.Direction = direction;
+            }
+
+            return parameter;
+        }
+    }
+}
