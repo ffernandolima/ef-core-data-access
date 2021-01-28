26f4889ce062426331e0481061dee2416af732d0
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2021-01-28 11:49:27 -0300

    Renamed variable.

--- samples/EntityFrameworkCore.WebAPI/Startup.cs
+++ samples/EntityFrameworkCore.WebAPI/Startup.cs
@@ -121,8 +121,8 @@ namespace EntityFrameworkCore.WebAPI
                 var appPath = AppDomain.CurrentDomain.BaseDirectory;
 
                 var entryAssembly = Assembly.GetEntryAssembly();
-                var aseemblyName = entryAssembly?.GetName();
-                var appName = aseemblyName?.Name;
+                var assemblyName = entryAssembly?.GetName();
+                var appName = assemblyName?.Name;
 
                 var filePath = Path.Combine(appPath, $"{appName ?? "EntityFrameworkCore.WebAPI"}.xml");
 
