5e05e14c9386bbc0dac5de16acff1e86297571aa
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2021-08-25 15:22:37 -0300

    Added the possibility to register repositories into the DI container and also let it control the repository lifetimes when possible.

---
+++ src/EntityFrameworkCore.Repository/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,86 @@
+﻿using EntityFrameworkCore.Repository.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace EntityFrameworkCore.Repository.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCustomRepository<TService, TImplementation>(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+            where TService : class, IRepository
+            where TImplementation : class, TService
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services), $"{nameof(services)} cannot be null.");
+            }
+
+            if (!(typeof(TImplementation).BaseType?.IsGenericType(typeof(Repository<>)) ?? false))
+            {
+                throw new ArgumentException("Implementation constraint has not been satisfied.");
+            }
+
+            switch (serviceLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    {
+                        services.TryAddSingleton<TService, TImplementation>();
+                    }
+                    break;
+                case ServiceLifetime.Scoped:
+                    {
+                        services.TryAddScoped<TService, TImplementation>();
+                    }
+                    break;
+                case ServiceLifetime.Transient:
+                    {
+                        services.TryAddTransient<TService, TImplementation>();
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepository<TService, TImplementation>(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+            where TService : class, IRepository
+            where TImplementation : class, TService
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services), $"{nameof(services)} cannot be null.");
+            }
+
+            if (!typeof(TImplementation).IsGenericType(typeof(Repository<>)))
+            {
+                throw new ArgumentException("Implementation constraint has not been satisfied.");
+            }
+
+            switch (serviceLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    {
+                        services.TryAddSingleton<TService, TImplementation>();
+                    }
+                    break;
+                case ServiceLifetime.Scoped:
+                    {
+                        services.TryAddScoped<TService, TImplementation>();
+                    }
+                    break;
+                case ServiceLifetime.Transient:
+                    {
+                        services.TryAddTransient<TService, TImplementation>();
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return services;
+        }
+    }
+}
