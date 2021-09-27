dba2cb78e8bf89dba42afe567f8ea99703e3451f
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2021-09-27 18:31:28 -0300

    Created abstractions and tweaked their references; Set lang version as the latest one to all projects; Syntax enhancements; Used pattern matching; Updated dependencies; Updated README.md; New package versions.

---
+++ src/EntityFrameworkCore.QueryBuilder.Abstractions/Interfaces/ISorting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace EntityFrameworkCore.QueryBuilder.Interfaces
+{
+    public interface ISorting<T> : ISorting
+    {
+        Expression<Func<T, object>> KeySelector { get; }
+    }
+
+    public interface ISorting
+    {
+        string FieldName { get; }
+        SortDirection SortDirection { get; }
+    }
+}
