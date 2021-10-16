92f4f3177b0aff8bec1d8e51f931eaa75487794b
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2021-10-16 17:18:33 -0300

    Added EnsureAutoHistory overloads.

---
+++ src/EntityFrameworkCore.AutoHistory/TrackedEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+
+namespace EntityFrameworkCore.AutoHistory
+{
+    public class TrackedEntity
+    {
+        public EntityEntry EntityEntry { get; }
+        public EntityState EntityState { get; }
+
+        public TrackedEntity(EntityEntry entityEntry)
+        {
+            EntityEntry = entityEntry ?? throw new ArgumentNullException(nameof(entityEntry), $"{nameof(entityEntry)} cannot be null.");
+            EntityState = entityEntry.State;
+        }
+    }
+}
