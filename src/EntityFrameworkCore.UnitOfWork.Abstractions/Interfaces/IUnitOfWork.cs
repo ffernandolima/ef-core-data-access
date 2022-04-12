a0997f5728684b77c1eb587460dd5cca0b78aa92
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2022-04-12 11:29:06 -0300

    Added support to pooled context.

--- src/EntityFrameworkCore.UnitOfWork.Abstractions/Interfaces/IUnitOfWork.cs
+++ src/EntityFrameworkCore.UnitOfWork.Abstractions/Interfaces/IUnitOfWork.cs
@@ -11,4 +11,9 @@ namespace EntityFrameworkCore.UnitOfWork.Interfaces
 
     public interface IUnitOfWork<T> : IUnitOfWork, ISyncUnitOfWork<T>, IAsyncUnitOfWork<T>, IDisposable where T : DbContext
     { }
+
+    public interface IPooledUnitOfWork<T> : IUnitOfWork<T>, IDisposable where T : DbContext
+    {
+        IDbContextFactory<T> DbContextFactory { get; }
+    }
 }
